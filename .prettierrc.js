fe29d2e97775e3588e0a333684d9fae5ab92d266
Julia Pyshko <julia.pyshko@gmail.com>
2023-02-20 01:21:46 +0500

    [Module-2] Added serverless plugins and .yml

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = { 
+    //arrowParens: 'avoid', 
+    //bracketSameLine: true, 
+    //bracketSpacing: true, 
+    //jsxBracketSameLine: true, 
+    singleQuote: false, 
+    //trailingComma: 'all',
+    endOfLine: "lf"
+ };
