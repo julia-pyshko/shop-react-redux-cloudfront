090290a296923496f19ae81157fa5cfec42b9c8a
Julia Pyshko <julia.pyshko@gmail.com>
2023-02-24 02:21:57 +0500

    [Module-3] Reconfigure react app to dynamic API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://kbol5ssuvl.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://kbol5ssuvl.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://kbol5ssuvl.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://kbol5ssuvl.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://kbol5ssuvl.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
