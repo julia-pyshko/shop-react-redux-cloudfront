770859b66c16de29446054de2351876e7b81c2a4
Julia Pyshko <julia.pyshko@gmail.com>
2023-02-25 23:06:43 +0500

    [Module 3] Fixed API paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://kbol5ssuvl.execute-api.us-east-1.amazonaws.com/dev",
-  order: "https://kbol5ssuvl.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://kbol5ssuvl.execute-api.us-east-1.amazonaws.com/dev",
-  bff: "https://kbol5ssuvl.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "https://kbol5ssuvl.execute-api.us-east-1.amazonaws.com/dev",
+  product: "https://c7f5siytb3.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://c7f5siytb3.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://c7f5siytb3.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://c7f5siytb3.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://c7f5siytb3.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
